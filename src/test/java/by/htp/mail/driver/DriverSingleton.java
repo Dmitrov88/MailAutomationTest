c851690b6885a703872c5ab2fdd1fa9d7c4a9eb9
Dmitrov Slava <dmitrov.v88@gmail.com>
2018-07-25 21:13:56 +0300

    DriverSingleton created

--- src/test/java/by/htp/mail/driver/DriverSingleton.java
+++ src/test/java/by/htp/mail/driver/DriverSingleton.java
@@ -1,5 +1,31 @@
 package by.htp.mail.driver;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class DriverSingleton {
 
+    private static WebDriver driver;
+    private static final String CHROME = "webdriver.chrome.driver";
+	private static final String CHROME_PATH = "C:\\WebDrivers\\chromedriver_win32\\chromedriver.exe";
+	
+
+    private DriverSingleton(){};
+    
+    public static WebDriver getDriver(){
+        if (driver == null){
+        	System.setProperty(CHROME, CHROME_PATH);
+        	WebDriver driver = new ChromeDriver();
+        	System.out.println("Driver started!");
+        }
+        
+        return driver;
+
+    }
+    
+    public static void closeDriver(){
+        driver.quit();
+        driver = null;
+    }
+    
 }
