9821dcfaf4815367a741b789432f2e1066352c27
Dmitrov Slava <dmitrov.v88@gmail.com>
2018-07-25 21:54:02 +0300

    Class Steps created

--- src/test/java/by/htp/mail/steps/Steps.java
+++ src/test/java/by/htp/mail/steps/Steps.java
@@ -1,5 +1,30 @@
 package by.htp.mail.steps;
 
+import org.openqa.selenium.WebDriver;
+
+import by.htp.mail.pages.LoginPage;
+
+import by.htp.mail.driver.DriverSingleton;
+
 public class Steps {
+	
+	private WebDriver driver;
+	
+	public void initBrowser()
+	{
+		driver = DriverSingleton.getDriver();
+	}
+	
+	public void closeDriver()
+	{
+		DriverSingleton.closeDriver();
+	}
+	
+	public void loginMail(String username, String password)
+	{
+		LoginPage loginPage = new LoginPage(driver);
+		loginPage.openPage();
+		loginPage.login(username, password);
+	}
 
 }
