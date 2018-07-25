21873bb74e117892dfbcdd5512cc687a5bd4b7e1
Dmitrov Slava <dmitrov.v88@gmail.com>
2018-07-25 21:44:57 +0300

    Class LoginPage created

--- src/test/java/by/htp/mail/pages/LoginPage.java
+++ src/test/java/by/htp/mail/pages/LoginPage.java
@@ -1,5 +1,44 @@
 package by.htp.mail.pages;
 
-public class LoginPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+import by.htp.mail.pages.AbstractPage;
+
+public class LoginPage extends AbstractPage {
+	
+	private final String BASE_URL = "http://www.mail.ru";
+	
+	public LoginPage(WebDriver driver)
+	{
+		super(driver);
+		//PageFactory.initElements(this.driver, this);
+	}
+	
+	@Override
+	public void openPage()
+	{
+		driver.get(BASE_URL);
+		
+	}
+	
+	@FindBy(xpath = ".//input[@id='mailbox:login']")
+	private WebElement login;
+
+	@FindBy(xpath = ".//input[@id='mailbox:pass']")
+	private WebElement pass;
+
+	@FindBy(xpath = ".//input[@id='mailbox:submit']")
+	private WebElement submit;
+	
+	public void login(String username, String password)
+	{
+		login.sendKeys(username);
+		pass.sendKeys(password);
+		submit.click();
+	}
+	
+	
 }
